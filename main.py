25c80738a39b34bce8a306617c85341d8fb80912
CrashyyOveride <263252668+CrashyyOveride@users.noreply.github.com>
2026-08-03 18:23:32 +1000

    Refactor: Remove character and weapon classes
    Removed character and weapon classes along with their instances.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random
+import game_logic
 
 class Game:
     def __init__(self, root):
@@ -8,6 +10,7 @@ class Game:
         self.root.attributes("-fullscreen", True)
         
         self.root.bind("<Escape>", lambda event: self.root.destroy())
+
 
         self.default_menu = (
             "Test Game\n\n\n"
@@ -62,121 +65,6 @@ class Game:
             self.menu_label.config(text=self.default_menu)
         else:
             self.menu_label.config(text=f"INVALID CHOICE: '{choice}'\n\nPlease select 1, 2, or 3.")
-
-class Character:
-    def __init__(self, name: str, health: int, attack_power: int):
-        self.name = name
-        self.max_health = health
-        self.current_health = health
-        self.base_attack = attack_power
-        self.is_alive = True
-        self.equipped_weapon = None
-
-    def get_attack_power(self):
-        if self.equipped_weapon:
-            return self.base_attack + self.equipped_weapon.bonus_damage
-        return self.base_attack
-
-    def take_damage(self, amount: int):
-        self.current_health -= amount
-        if self.current_health <= 0:
-            self.current_health = 0
-            self.is_alive = False
-            
-class Ally(Character):
-    def __init__(self, name: str, health: int, attack_power: int, can_use_magic: bool):
-        super().__init__(name, health, attack_power)
-        self.can_use_magic = can_use_magic # Exiled will be False, anyone else will be True
-
-    def heal_target(self, target: Character, amount: int):
-        if self.is_alive and target.is_alive:
-            target.current_health = min(target.max_health, target.current_health + amount)
-
-class Enemy(Character):
-    def __init__(self, name: str, health: int, attack_power: int, is_boss: bool, primary_zone: str ="Any"):
-        super().__init__(name, health, attack_power)
-        self.is_boss = is_boss
-        self.primary_zone = primary_zone
-        self.is_attackable = True
-
-        def trigger_status_effect(self):
-            """Placeholder"""
-            pass
-
-class Weapon:
-    def __init__(self, name: str, bonus_damage: int, rarity: str):
-        self.name = name
-        self.bonus_damage = bonus_damage
-        self.rarity = rarity
-        self.is_relic = False
-        self.orsted_apperance = False
-
-class Relic(Weapon):
-    def __init__(self, name: str, bonus_damage: int, rarity: str, orsted_apperance: bool, is_relic: bool):
-        super().__init__(name, bonus_damage, rarity)
-
-        self.is_relic = is_relic
-        self.orsted_apperance = orsted_apperance
-
-### HERE ARE ALL CHARACTERS :)))
-
-player = Ally(name="Exiled", health=100, attack_power=15, can_use_magic=False)
-ernest = Ally(name="Ernest Blackwood", health=120, attack_power=18, can_use_magic=True)
-mary = Ally(name="Mary Althea", health=9999, attack_power=0, can_use_magic=True)
-
-bandit = Enemy(name="Gilded Gold Underling", health=30, attack_power=8, is_boss=False)
-orsted = Enemy(name="Orsted Drake", health=999999999999999, attack_power=99999999999, is_boss=True)
-orsted.is_attackable = False
-
-soldat =  Enemy(name="Soldat Vanderbilt", health=130, attack_power=30, is_boss=False)
-
-### Dungeon 1 Monsters & Boss
-orb = Enemy(name="Orb", health=20, attack_power=5, is_boss=False)
-noid = Enemy(name="Noid", health=45, attack_power=12, is_boss=False)
-lesser_dragon = Enemy(name="Lesser Dragon", health=350, attack_power=40, is_boss=True)
-
-### Dungeon 2 Monsters & Boss
-warden = Enemy(name="Deeproot Warden", health=90, attack_power=25, is_boss=False)
-shadow = Enemy(name="Shadowroot Spirits", health=40, attack_power=30, is_boss=False)
-draconic = Enemy(name="Draconic Gloomtree Sentinel", health=550, attack_power=45, is_boss=True)
-
-### Dungeon 3 Monsters & Boss
-
-### HERE ARE ALL WEAPONS :)))
-
-### Common Weapons
-dagger = Weapon(name="Dagger", bonus_damage=5, rarity="Common")
-shield = Weapon(name="Shield", bonus_damage=5, rarity="Common")
-longsword = Weapon(name="Longsword", bonus_damage=5, rarity="Common")
-greatsword = Weapon(name="Greatsword", bonus_damage=5, rarity="Common")
-stick = Weapon(name="Stick", bonus_damage=0, rarity="Common")
-
-### Rare Weapons
-kris = Weapon(name="Holy Kris", bonus_damage=10, rarity="Rare")
-vantablack = Weapon(name="Vantablack Great Axe", bonus_damage=15, rarity="Rare")
-twinlight = Weapon(name="Twinlight Odachi", bonus_damage=20, rarity="Rare")
-thorn = Weapon(name="Gilded Thorn", bonus_damage=20, rarity="Rare")
-
-### Legacy Weapons
-gravewarden = Weapon(name="Gravewarden", bonus_damage=50, rarity="Legacy")
-silencedagger = Weapon(name="Parthanlán's Silence", bonus_damage=50, rarity="Legacy")
-glacialhilt = Weapon(name="Replica Glacial Hilt", bonus_damage=20, rarity="Legacy")
-
-### Relics
-
-goditem = Relic(name="Matej's Old Axe", bonus_damage=999999999, rarity="Relic", orsted_apperance=False, is_relic=False)
-greatrune = Relic(name="Orsted's Great Rune", bonus_damage=0, rarity="Relic", orsted_apperance=True, is_relic=True)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
